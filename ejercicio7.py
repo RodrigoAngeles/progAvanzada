2f280e17cf04d896683c8d968405887e69f40b8f
RodrigoAngeles <55461755+RodrigoAngeles@users.noreply.github.com>
2019-09-19 12:37:03 -0500

    Update ejercicio7.py

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,5 +1,6 @@
-# escriba un program que lea un numero positivo (n), insetado por el usuario y despues despliegue la suma de todos los enteros desde 1 hasta n.
-# Lasuma de los primeros enteros n positivos pueden ser calculando usando la formula (n)(n+1) / 2
+# Ejercicio 7
+# Escriba un program que lea un numero positivo (n), insetado por el usuario y despues despliegue la suma de todos los enteros desde 1 hasta n.
+# La suma de los primeros enteros n positivos pueden ser calculando usando la formula (n)(n+1) / 2
 
 numero = int(input('Inserte numero positivo: '))
 
