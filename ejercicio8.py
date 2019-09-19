259c7a74fc8413f6c35e3b1f7a81f5ecdf5f30cb
RodrigoAngeles <55461755+RodrigoAngeles@users.noreply.github.com>
2019-09-19 12:39:00 -0500

    Update ejercicio8.py

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,7 +1,8 @@
-# un vendedor de una pagina de abarrotes en linea vende dos tipos de caja de cereal.
-# cornflakes de 750gr y trix 500gr.
-# escriba un programa que lea el numero de cajas cornflakes y de trix, cuyo valor debe ser introducido por el usuario.
-# despues su programa debe calcular y mostrar el total del peso ( en kilogranmos) del envio.
+# Ejercico 8
+# Un vendedor de una pagina de abarrotes en linea vende dos tipos de caja de cereal.
+# Cornflakes de 750gr y trix 500gr.
+# Escriba un programa que lea el numero de cajas cornflakes y de trix, cuyo valor debe ser introducido por el usuario.
+# Despues su programa debe calcular y mostrar el total del peso ( en kilogranmos) del envio.
 
 corn = float(input('Cuantas cajar de cornflakes necesita: '))
 
