a11199e80ac3292fb6029bf77478c74a02ee5f4a
RodrigoAngeles <55461755+RodrigoAngeles@users.noreply.github.com>
2019-09-19 12:54:50 -0500

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,6 +1,6 @@
 # Ejercicio 3
-# Escriba un programa que le pregunte al usuario el largo y el ancho de una habitacion. 
-# Una vez qe los valores an sido leidos, su programa debe calcular y desplegar el area de la habitacion. 
+# Escriba un programa que le pregunte al usuario el largo y el ancho de una habitación. 
+# Una vez que los valores han sido leídos, su programa debe calcular y desplegar el área de la habitación. 
 # El largo y ancho debe ser introducido con punto flotante (decimal). 
 # Incluya las unidades (metros) en su mensaje de entrada y salida.
 
@@ -8,4 +8,5 @@
 largo = float(input ('inserta el largo de la habitacion: '))
 ancho = float(input ('inserta el ancho de la habitacion: '))
 
-print ('\n El area de la habitaciones de', largo*ancho, 'Metros cuadrados')
+print ('\n El area de la habitación es de', largo*ancho, 'Metros cuadrados')
+
