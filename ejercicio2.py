ff53bea994980d95e260ac57897dd46f1c455510
RodrigoAngeles <55461755+RodrigoAngeles@users.noreply.github.com>
2019-09-19 12:56:16 -0500

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,6 +1,6 @@
 # Ejercicio 2
 # Escribir un programa que le pregunte al usuario por su nombre. 
 # El programa debe responder con un mensaje que le diga hola usuario, usando su nombre.
-# \n salto de pagina, \t salto de tabulacion.
+# \n salto de página, \t salto de tabulación.
 nombre = input('¿Cual es tu nombre?: ')
 print ('\n Hola Bienvenido', nombre)
