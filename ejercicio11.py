27eb30831e8d53d2a1ab683d120c00940be015d5
RodrigoAngeles <55461755+RodrigoAngeles@users.noreply.github.com>
2019-09-22 16:40:24 -0500

    Update ejercicio11.py

--- ejercicio11.py
+++ ejercicio11.py
@@ -3,11 +3,12 @@
 # En México, la eficiencia del combustible normalmente se expresa en litros por cien kilómetros (L / 100 km). 
 # Usa tus habilidades de investigación para determinar cómo convertir de MPG a L / 100 km. 
 # Luego cree un programa que lea un valor del usuario en unidades estadounidenses y muestre la eficiencia de combustible equivalente en unidades mexicanas.
-# convercion 1 mpg = 235.21 l/100km, la conversion seria es 235.21/ MPG para obtener l/100km
+# conversión 1 mpg = 235.21 l/100km, la conversión seria es 235.21/ MPG para obtener l/100km
 
 eu = float(input('la eficiencia en Estados unidos de MPG es: '))
 
-convercion = 235.21 / eu 
+conversion = 235.21 / eu 
 
-print('\n La eficiencia de conbustible en Mexico es: %.2f' % convercion, 'L/100km' )
+print('\n La eficiencia de combustible en Mexico es: %.2f' % conversion, 'L/100km' )
+
 
